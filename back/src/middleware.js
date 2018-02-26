baaf2fb9ff0b919c2de8a9909c0dd6aa974cc482
younghea <dydgo9507@gmail.com>
2018-02-26 17:24:44 +0900

    middleware를 작성

---
+++ back/src/middleware.js
@@ -0,0 +1,46 @@
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const csurf = require('csurf');
+const cookieSession = require('cookie-session');
+
+const corsMiddleware = cors({
+  origin: process.env.TARGET_ORIGIN
+});
+
+const bodyParserJsonMiddleware = bodyParser.json();
+
+const bodyParserUrlEncodedMiddleware = bodyParser.urlencoded({ extended: false });
+
+const csrfMiddleware = csurf();
+
+const cookieSessionMiddleware = cookieSession({
+  name: 'bubble',
+  keys: [
+    process.env.SESSION_SECRET
+  ],
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 8
+  }
+});
+
+const insertToken = (req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+};
+
+const checkAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  }
+  res.redirect('/user/login');
+};
+
+module.exports = {
+  corsMiddleware,
+  bodyParserJsonMiddleware,
+  bodyParserUrlEncodedMiddleware,
+  csrfMiddleware,
+  cookieSessionMiddleware,
+  insertToken,
+  checkAuthenticated
+}
