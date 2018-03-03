8533f9162c4224e8fa6ec4966a8d9fd73d0d3204
younghea <dydgo9507@gmail.com>
2018-03-03 16:50:59 +0900

    미들웨어를 작성하고 passport 파일을 새로 만들어 따로 정리하였다.

--- src/user/user.js
+++ src/user/user.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
 
 // 컨트롤러를 통해서 실행해
 const register = require('./user.register.controller');
@@ -6,19 +9,21 @@ const mw = require('../middleware');
 const query = require('../Query');
 
 const app = express();
+require('./passport')(passport);
 
 const router = express.Router();
 
 // middleware
+router.use(mw.bodyParserJsonMiddleware);
+router.use(mw.bodyParserUrlEncodedMiddleware);
+router.use(mw.corsMiddleware);
+router.use(mw.cookieSessionMiddleware);
+router.use(mw.csrfMiddleware);
+router.use(mw.insertToken);
 
-// passport
-
-// Passport Serializer
-
-// Passport Deserializser
-
-
-// Local Strategy
+// passport 초기화
+router.use(passport.initialize());
+router.use(passport.session());
 
 router.post('/register', register.try);
 
