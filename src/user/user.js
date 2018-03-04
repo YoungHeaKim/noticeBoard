d7a1558dbd4f0adf7e78f905657bb52395b6545c
younghea <dydgo9507@gmail.com>
2018-03-04 20:14:26 +0900

    쿼리를 사용하여 작성

--- src/user/user.js
+++ src/user/user.js
@@ -31,12 +31,13 @@ passport.use('signin', new LocalStrategy({
   passwordField: 'password',
   passReqToCallback: true
 }, function (req, email, password, done) {
-  User.findOne({ 'email': email }, function (err, user) {
-    if (err) return done(err);
-    if (!user) return done(null, false, req.flash('signinMessage', '아이디가 존재하지 않습니다.'));
-    if (!user.validPassword(password)) return done(null, false, req.flash('signinMessage', '비밀번호가 올바르지 않습니다'));
-    return done(null, user);
-  });
+  query.findUserByEmail({ email })
+    .then( function (err, user) {
+      if (err) return done(err);
+      if (!user) return done(null, false, req.flash('signinMessage', '아이디가 존재하지 않습니다.'));
+      if (!user.validPassword(password)) return done(null, false, req.flash('signinMessage', '비밀번호가 올바르지 않습니다'));
+      return done(null, user);
+        });
 }));
 
 router.post('/register', register.try);
--- src/user/user.register.controller.js
+++ src/user/user.register.controller.js
@@ -12,20 +12,25 @@ exports.try = (req, res) => {
     passwordField: 'password',
     passReqToCallback: true
   }, function (req, email, password, done) {
-    User.findOne({ 'email': email }, function (err, user) {
-      if (err) return done(null);
-      if (user) return done(null, false, req.flash('signupMessage', '중복된 아이디입니다.'));
+    query.findUserByEmail({ email })
+      .then( function (err, user) {
+        if (err) {
+          return done(null)
+        };
+        if (user) {
+          return done(null, false, req.flash('signupMessage', '중복된 아이디입니다.'))
+        }
+ 
+        query.createUserByEmail({ email, password, name})
+          .then(newUser => {
+            newUser.email = req.body.email;
+            newUser.password = newUser.generateHash(password);
+            newUser.name = req.body.name;
+          })
+          .then(() => {
+            res.redirect('/user/login');
+          })
 
-      const newUser = new User();
-
-      newUser.email = email;
-      newUser.password = newUser.generateHash(password);
-      newUser.name = req.body.name;
-
-      newUser.save(function (err) {
-        if (err) throw err;
-        return done(null, newUser);
-      });
     })
   }));
 };
