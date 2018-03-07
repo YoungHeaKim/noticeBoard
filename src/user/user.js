18c11cc0ce60b25573a691aa1cb0c4b542043406
younghea <dydgo9507@gmail.com>
2018-03-07 14:41:47 +0900

    passport파일을 삭제하고 파일 안에 직접 적어주었다.

--- src/user/user.js
+++ src/user/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const flash = require('connect-flash');
 
 // 컨트롤러를 통해서 실행해
 const register = require('./user.register.controller');
@@ -9,7 +11,6 @@ const mw = require('../middleware');
 const query = require('../Query');
 
 const app = express();
-require('./passport')(passport);
 
 const router = express.Router();
 
@@ -18,26 +19,83 @@ router.use(mw.bodyParserJsonMiddleware);
 router.use(mw.bodyParserUrlEncodedMiddleware);
 router.use(mw.corsMiddleware);
 router.use(mw.cookieSessionMiddleware);
-router.use(mw.csrfMiddleware);
-router.use(mw.insertToken);
+// router.use(mw.csrfMiddleware);
+// router.use(mw.insertToken);
+
+// jwt발급
+const oauthHandler = (req, res) => {
+  const token = jwt.sign({
+    id: req.user.id,
+    expiresIn: '1d'
+  }, process.env.SECRET);
+  const origin = process.env.TARGET_ORIGIN;
+  res.send(`<script>window.opener.postMessage('${token}', '${origin}')</script>`);
+}
 
 // passport 초기화
 router.use(passport.initialize());
 router.use(passport.session());
+router.use(flash())
 
-// 로그인 
-passport.use('signin', new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password',
+// passport serialize
+passport.serializeUser(function (user, done) {
+  console.log('SerializeUser');
+  done(null, user.id);
+});
+
+// passport deserialize
+passport.deserializeUser(function (id, done) {
+  console.log('DeserializeUser');
+  query.findById({ id })
+    .then(
+      (err, user) => {
+        done(err, user);
+      });
+});
+
+// passport local-login
+passport.use('login', new LocalStrategy({
+  usernameField : 'email',
+  passwordField : 'password',
   passReqToCallback: true
-}, function (req, email, password, done) {
-  query.findUserByEmail({ email })
-    .then( function (err, user) {
-      if (err) return done(err);
-      if (!user) return done(null, false, req.flash('signinMessage', '아이디가 존재하지 않습니다.'));
-      if (!user.validPassword(password)) return done(null, false, req.flash('signinMessage', '비밀번호가 올바르지 않습니다'));
-      return done(null, user);
-        });
+},
+(req, email, password, done) => {
+  if(email) {
+    // 이메일을 작성시에 소문자만 사용하였는지 체크해주는 부분
+    email = email.toLowerCase();
+  }
+
+  process.nextTick( () => {
+    query.findByIdInLocal({ email })
+      .then((err, user) => {
+        if(err){
+          return done(err);
+        }
+
+        if(!user) {
+          return done(null, false, req.flash('loginMessage', '유저가 없습니다.'));
+        }
+
+        if(!user.validPasswoord(password)) {
+          return done(null, false, req.flash('loginMessage', 'Password가 틀렸습니다.'));
+        }
+
+        else {
+          return done(null, user);
+        }
+      });
+  });
+}));
+
+// 로그인
+router.get('/login', (req, res) => {
+  res.render('login.ejs', {message: req.flash('loginMessage') });
+});
+
+router.post('/login', passport.authenticate('login', {
+  successRedirect : '/article/lists',
+  failureRedirect : '/user/login',
+  failureFlash : true
 }));
 
 router.post('/register', register.try);
--- src/user/user.register.controller.js
+++ src/user/user.register.controller.js
@@ -7,31 +7,79 @@ const LocalStrategy = require('passport-local').Strategy;
 const router = express.Router();
 
 exports.try = (req, res) => {
-  // 회원가입
+  // passport local signUp
   passport.use('signup', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-  }, function (req, email, password, done) {
-    query.findUserByEmail({ email })
-      .then( function (err, user) {
-        if (err) {
-          return done(null)
-        };
-        if (user) {
-          return done(null, false, req.flash('signupMessage', '중복된 아이디입니다.'))
-        }
- 
-        query.createUserByEmail({ email, password, name})
-          .then(newUser => {
-            newUser.email = req.body.email;
-            newUser.password = newUser.generateHash(password);
-            newUser.name = req.body.name;
-          })
-          .then(() => {
-            res.redirect('/user/login');
-          })
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true
+  },
+  (req, email, password, name, done) => {
+    if (email) {
+      email = email.toLowerCase();
+    }
 
-    })
+    process.nextTick(() => {
+      if (!req.user) {
+        query.findByIdInLocal({ email })
+          .then((err, user) => {
+            if(err){
+              return done(err);
+            }
+
+            if(user) {
+              return done(null, false, req.flash('signupMessage', '이메일이 이미 등록되어 있습니다.'))
+            } else {
+              const newUser = query.findAllUser()
+                .then()
+              
+              newUser.local.email = email;
+              newUser.local.password = newUser.generateHash(password);
+              newUser.local.name = name;
+
+              newUser.save((err) => {
+                if(err) {
+                  return done(err);
+                }
+
+                return done(null, newUser);
+              });
+            }
+          })
+      } else if(!req.user.local.email) {
+        query.findByIdInLocal({email}, (err, user) => {
+          if(err) {
+            return done(err);
+          }
+
+          if(user) {
+            return done(null, false, req.flash('loginMessage', '이메일이 이미 등록되어 있습니다.'))
+          } else {
+            const user = req.user;
+            
+            user.local.email = email;
+            user.local.password = user.generateHash(password);
+            user.save((err) => {
+              if(err) {
+                return done(err);
+              }
+
+              return done(null, user);
+            });
+          }
+        });
+      } else {
+        return done(null, req.user);
+      }
+    });
+}));
+  // 회원가입
+  router.get('/register', (req, res) => {
+    res.render('register.ejs', { message: req.flash('signupMessage') });
+  })
+
+  router.post('/register', passport.authenticate('singup', {
+    successRedirect: '/user/login',
+    failureRedirect : '/user/register',
+    failureRedirect: true
   }));
 };
