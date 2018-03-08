f673797d1827eb662d5ec285b6d0334823325b60
younghea <dydgo9507@gmail.com>
2018-03-08 15:39:10 +0900

    쿼리 일괄 삭제

--- src/Query/AuthQuery.js
+++ src/Query/AuthQuery.js
@@ -1,14 +1,5 @@
 const User = require('../models/user');
 
 module.exports = {
-  findById({ id }) {
-    return User.findById(id)
-  },
-  findByIdInLocal({ email }) {
-    return User.findOne()
-      .where({'provide' : 'local', email})
-  },
-  findAllUser() {
-    return User.find()
-  }
+  
 };
