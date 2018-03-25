3dfdebb7a655d71923640d8a4dd94256d9c721fe
younghea <dydgo9507@gmail.com>
2018-03-25 18:29:27 +0900

    게시글을 등록하는 부분을 작성

--- src/article/article.post.controller.js
+++ src/article/article.post.controller.js
@@ -1,6 +1,15 @@
 const query = require('../Query');
 const ms = require('../message');
+const Article = require('../models/article');
 
-exports.try = (req, res) => {
-  
+exports.createList = async (req, res) => {
+  const articleInfo = {
+    title: req.body.title,
+    description: req.body.description
+  };
+  const article = await Article.create(articleInfo);
+  if(article) {
+    console.log('success')
+    return res.status(200).json('성공적으로 업데이트가 되었습니다.').redirect('/article/lists');
+  }
 };
