ccbd7a110cf3d3700aaa1eaf39796a68926923c0
younghea <dydgo9507@gmail.com>
2018-03-25 18:22:41 +0900

    article 삭제 부분 작성

--- src/article/article.delete.controller.js
+++ src/article/article.delete.controller.js
@@ -1,6 +1,13 @@
 const query = require('../Query');
 const ms = require('../message');
+const Article = require('../models/article');
 
-exports.try = (req, res) => {
-  
+exports.delete = (req, res) => {
+  Article.remove({_id: req.params.article_id}, (err) => {
+    if(err) {
+      return res.status(500).json({ error: "데이터베이스 연결상에 문제가 있습니다."});
+    }
+
+    res.status(204).redirect('/lists');
+  }) 
 };
