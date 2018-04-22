292ea81399e7cceb43125ca771505c946623423a
younghea <dydgo9507@gmail.com>
2018-04-22 16:53:12 +0900

    수정하는부분에 대해 고침

--- src/user/user.js
+++ src/user/user.js
@@ -17,11 +17,6 @@ router.get('/register', (req, res) => {
   res.render(path.join(__dirname, '../../views/login/register.ejs'));
 });
 
-// user 수정하는 페이지
-router.get('/edit', (req, res) => {
-  res.render(path.join(__dirname, '../../views/login/edit.ejs'));
-})
-
 // 쿠키 발행 부분
 router.post('/cookie', cookie.cookie);
 
@@ -31,7 +26,15 @@ router.get('/logout', cookie.cookieRemove);
 // 회원가입
 router.post('/register', register.signUp);
 
-// 수정하는 부분
+// user 수정하는 페이지
+router.get('/edit', (req, res) => {
+  res.render(path.join(__dirname, '../../views/login/edit.ejs'));
+})
+
+// 수정하는 부분(ejs에서)
+router.post('/edit', register.edit);
+
+// 수정하는 부분(back)
 router.put('/edit', register.edit);
 
 module.exports = router;
