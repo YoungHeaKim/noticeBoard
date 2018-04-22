000db793baeca324c3b0e93fb2a8b45b5f746fc8
younghea <dydgo9507@gmail.com>
2018-04-22 16:54:44 +0900

    수정하는 부분에서 오류가 나는 부분을 수정하여 코드 등록

--- src/user/user.register.controller.js
+++ src/user/user.register.controller.js
@@ -4,6 +4,7 @@ const express = require('express');
 const query = require('../Query');
 const ms = require('../message');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
@@ -12,9 +13,14 @@ exports.signUp = async (req, res) => {
     return res.status(400).json('이메일을 입력해주세요')
   } else if (!req.body.password) {
     return res.status(400).json('비밀번호를 입력해주세요')
+  } else if (!req.body.password2) {
+    return res.status(400).json('확인 비밀번호를 입력해주세요')
   } else if (!req.body.username) {
     return res.status(400).json('유저을 입력해주세요')
+  } else if ( req.body.password !== req.body.password2 ) {
+    return res.status(400).json('비밀번호와 확인 비밀번호를 똑같이 입력해주세요.')
   }
+
   const checkEmail = await query.checkIdExist(req.body.email);
   const checkUsername = await query.checkUsernameExist(req.body.username);
   if (checkEmail || checkUsername) {
@@ -31,42 +37,40 @@ exports.signUp = async (req, res) => {
   const createUser = await query.createUser(userInfo);
   if (createUser) {
     console.log('success')
-    return res.status(200).json('성공')
+    return res.status(200).redirect('/user/login');
   }
 };
 
 exports.edit = async (req, res) => {
-  if (!req.body.email) {
-    console.log('email오류')
-    return res.status(400).json('email이 맞지 않습니다.')
-  } else if (!req.body.password) {
-    console.log('password오류')    
-    return res.status(400).json('password가 맞지 않습니다.')
-  } else if (!req.body.username) {
-    console.log('username오류')    
-    return res.status(400).json('username이 맞지 않습니다.')
-  }
-  const userInfo = {
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 10),
-    username: req.body.username
-  };
-  // 이메일이 하나만 있는지 확인하는 부분
-  const checkEmail = await query.checkIdExist(userInfo.email);
-  const checkUsername = await query.checkUsernameExist(userInfo.username);
-  if (checkEmail === null) {
-    if(checkUsername) {
-      console.log('닉네임 오류');
-      res.status(400).json('닉네임이 이미 존재합니다.')
-    }
-    console.log('아이디나 패스워드 오류');
-    return res.status(400).json('아이디가 없습니다. \'id\' or \'password\'를 체크해주세요');
-  }
+  // 1. 로그인이 되어있는 정보 가져오기
+  const token = req.cookies.auth;
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const user = await query.checkUserBy_id(decoded.id);
+  const Password = user.password;
+  const checkPassword = req.body.checkPassword;
+  // 2. 로그인이 되어있는 유저의 비밀번호와 입력한 비번이 일치하는지 확인
+  const check = (user && bcrypt.compareSync(checkPassword, Password)) ? (null, user) : (null, false);
+  // 3. 확인이 되면 유저 정보를 수정
+  if (!check) {
+    console.log('password오류')
+    return res.status(400).json('확인 비밀번호가 맞지않습니다.')
+  } else {
+    // 4. 수정한 정보를 데이터베이스에 저장
+    const userInfo = {
+      email: user.email,
+      password: bcrypt.hashSync(req.body.newPassword, 10),
+      username: req.body.newUsername
+    };
 
-  // 확인이 완료된 userInfo를 데이터베이스에 생성하는 부분
-  const updateUser = await query.updatePasswordAndUsername(userInfo);
-  if (updateUser) {
-    console.log('수정완료');
-    return res.status(200).redirect('/article/lists')
-  }
+    // 이메일이 하나만 있는지 확인하는 부분
+    const checkUsername = await query.checkUsernameExist(userInfo.username);
+    if (checkUsername !== null) {
+      return res.status(400).json('닉네임이 이미 존재합니다.')
+    }
+    const updateUser = await query.updatePasswordAndUsername(user._id, userInfo)
+    if (updateUser) {
+      return res.status(200).redirect('/article/lists')
+    }
+    return res.status(400).json('수정되지 않았습니다.');
+  } 
 }
