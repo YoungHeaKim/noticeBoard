fbdf1d091d9c0d41f2a47ab7bdac020fdd918b6b
younghea <dydgo9507@gmail.com>
2018-04-24 17:29:01 +0900

    게시글 삭제기능 완료

--- src/article/article.delete.controller.js
+++ src/article/article.delete.controller.js
@@ -2,12 +2,10 @@ const query = require('../Query');
 const ms = require('../message');
 const Article = require('../models/article');
 
-exports.delete = (req, res) => {
-  Article.remove({_id: req.params.article_id}, (err) => {
-    if(err) {
-      return res.status(500).json({ error: "데이터베이스 연결상에 문제가 있습니다."});
-    }
-
-    res.status(204).redirect('/lists');
-  }) 
+exports.delete = async (req, res) => {
+  const article = await query.removeArticle({_id: req.params._id})
+  if(!article) {
+    return res.status(500).json({ error: "데이터베이스 연결상에 문제가 있습니다."});
+  }
+  res.status(204).redirect('/article/lists');
 };
