413282e18523bf11e44fe48e4603761e1f14743d
younghea <dydgo9507@gmail.com>
2018-05-01 16:35:50 +0900

    session부분 추가 및 req.user를 가져오는 부분 추가

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const http = require('http');
 const path = require('path');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const server = http.Server(app);
 global.db = mongoose.createConnection(process.env.MONGO_URI);
@@ -21,7 +22,6 @@ const Article = require('./article/article');
 // cookie체크부분
 const checker = require('./user/user.access.controller');
 
-
 // ejs 템플릿
 app.set('view engine', 'ejs');
 
@@ -30,16 +30,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(morgan('dev'));
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false
+  }
+}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// 로그인정보를 뷰에서만 변수로 셋팅
+app.use((req, res, next) => {
+  app.locals.isLogin = req.isAuthenticated();
+  app.locals.userData = req.user;  
+  next()
+})
+
 app.use('/user', User);
 app.use('/article', checker.accessChecker, Article);
 // 기본페이지를 리스트페이지로 변환
 app.use('/', (req, res) => {
   return res.redirect('/article/lists');
 });
-
-// Passport
-app.use(passport.initialize());
-app.use(passport.session());
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Successfully connected to mongodb'))
